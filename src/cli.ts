c334f368f0dbc17953ad6beae919424f5974004f
Nona Takahara <45140117+nona-takahara@users.noreply.github.com>
2024-11-24 15:15:53 +0900

    Hotfix: v0.1.2

--- src/cli.ts
+++ src/cli.ts
@@ -9,7 +9,7 @@ import { Minifier, MinifierMode } from "./minifier";
 const program = new Command();
 
 program
-  .version("0.1.1")
+  .version("0.1.2")
   .description("A Lua minifier also outputs source map")
   .option(
     "-m, --module-like-lua",
